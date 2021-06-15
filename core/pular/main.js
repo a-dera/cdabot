815cebdab1acf6fbca8564ebc4ef920d39326333
guireelisee <guireelisee@gmail.com>
2021-06-15 19:01:32 +0000

    traduction pular

---
+++ core/pular/main.js
@@ -0,0 +1,333 @@
+
+/*------ -------------Section main--------*/
+var mainFrench = function() {
+  botui.action.button({
+    delay: 1000,
+    action: [{
+      text: 'En savoir plus sur Cluster Digital Africa',
+      _icon: 'info',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'cda'
+    }, {
+      text: 'Naatugol',
+      _icon: 'rocket',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'adhesion'
+    },{
+      text: 'Je ne trouve pas ce que je cherche',
+      _icon: 'search',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'search'
+    },{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }]
+  }).then(function (res) {
+      if(res.value == 'cda') {
+        presentation();
+      } else if(res.value == 'adhesion') {
+        adhesion();
+      } else if(res.value == 'search') {
+        chitchat();
+      } else {
+        start();
+      }
+  });
+
+
+}
+
+/*-------------------------------- fin section -----------------------------------*/
+
+
+/*------ -------------Section  Infoarmtations générales sur CDA-------*/
+var presentation = function() {
+
+    botui.message.bot({ // TODO #2 #1 Correction des icones
+    delay: 500,
+    type:'html',
+    content: 'Le Cluster permet aux entreprises de se regrouper et d’être représentées vis-à -vis de l’écosystème institutionnel. Adhérer le Cluster, c’est vous permettre de partager vos expériences & vos actualités avec le collectif et parfois élaborer des programmes collectifs. Adhérer le cluster, c’est valoriser l’excellence Numérique en Afrique.'
+      }).then(function () {
+    return botui.action.button({
+    delay: 1000,
+    action: [{
+      text: 'Laamu',
+      _icon: 'crown',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'gouvernance'
+    }, {
+      text: 'Birol nadaraniibe golleéji',
+      _icon: 'pen',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'bureau'
+    },{
+      text: 'Ekip',
+      _icon: 'team',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'team'
+    },{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }]
+  }).then(function (res) {
+      if(res.value == 'gouvernance') {
+        gouvernance();
+      } else if(res.value == 'bureau') {
+        bureau();
+      } else if(res.value == 'team') {
+        team();
+      } else {
+        mainFrench();
+      }
+  })
+  });
+
+}
+
+var gouvernance = function() {
+  botui.message.bot({
+    delay: 500,
+    type:'html',
+    content: '<ul><li><strong>Hooreejo </strong>Amadou DIAWARA</li>\
+                  <li><strong>Hooreejo teddinaado </strong>Mossadeck BALLY</li>\
+                  <li><strong>Hooreejo teddinaado </strong>Mme Mbaranga Gasarabwe</li>\
+                  <li><strong>Hooreejo teddinaado </strong>Messaoud Prosper AMOUYAL</li>\
+                  <li><strong>Ndewtudo Hooreejo </strong>Patrick GIVANOVICTH</li>\
+                  <li><strong>Ndewtudo Hooreejo honorifique</strong>Kacem AIYTYALLA</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Balkissa Idé SIDDO</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Diane Malikane Sugira</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Diadié dit Amadou SANKARE</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Alima DIAWARA</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Djénéba GORY</li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Mouminatou KANTE/li>\
+                  <li><strong>Ndewtudo Hooreejo teddinaado</strong>Fatoumata TRAORE</li><br>\
+              </ul>'
+      }).then(function () {
+      return botui.message.bot({
+        delay: 1000,
+        loading: true,
+        type:'html',
+        content: '<a href="https://clusterdigitalafrica.com/gouvernance/" target="_blank"> Plus de détails sur la gouvernance ici</a>'
+      });
+    }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }
+    ]
+    })
+  }).then(function (res) {
+  if(res.value == 'skip') {
+    presentation();
+  } else{
+    presentation();
+  }
+  });
+}
+
+var bureau = function() {
+   botui.message.bot({
+    delay: 500,
+    type:'html',
+    content: '<ul><li><strong>Hooreejo </strong>Amadou DIAWARA</li>\
+                  <li><strong>Sekerter jeneral </strong>Karim RAHHAOUI</li>\
+                  <li><strong>Kawjido djawdi </strong>Mamadou DOUMBIA</li>\
+                  <li><strong>Kordinater jeneral </strong>Binatou Coulibaly</li>\
+              </ul>'
+      }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }
+    ]
+    })
+  }).then(function (res) {
+  if(res.value == 'skip') {
+    presentation();
+  } else{
+    presentation();
+  }
+  });
+}
+
+var team = function() {
+  botui.message.bot({
+        delay: 1000,
+        loading: true,
+        type:'html',
+        content: '<a href="https://clusterdigitalafrica.com/gouvernance/" target="_blank"> Cliquez vpour vous rendre à la page dédié </a>'
+      }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }
+    ]
+    })
+  }).then(function (res) {
+  if(res.value == 'skip') {
+    presentation();
+  } else{
+    presentation();
+  }
+  });
+}
+
+
+
+/*------ -------------Section  Renseignement pour adhérer--------*/
+var adhesion = function() {
+     botui.action.button({
+    delay: 1000,
+    action: [{
+      text: 'Comment adhérer?',
+      _icon: 'how',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'how'
+    }, {
+      text: 'Kossabi natude ?',
+      _icon: 'question',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'why'
+    },{
+      text: 'Conditions d\'adhésion',
+      _icon: 'list',
+      get icon() {
+        return this._icon;
+      },
+      set icon(value) {
+        this._icon = value;
+      },
+      value: 'conditions'
+    },{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }]
+  }).then(function (res) {
+      if(res.value == 'how') {
+        comment();
+      } else if(res.value == 'why') {
+        pourquoi();
+      } else if(res.value == 'conditions') {
+        condions();
+      } else {
+        mainFrench();
+      }
+  });
+
+}
+
+var comment = function() {
+
+
+
+}
+
+var pourquoi = function() {
+
+  botui.message.bot({ // TODO #2 #1 Correction des icones
+    delay: 500,
+    type:'html',
+    content: '<ul><li><span class="icon"><i class="fas fa-minus"></i></span>Hokkugol yièdè ley, leydi ndewgu, leydè adunaaru  antarpiriisi djimone</li>\
+                  <li><span class="icon"><i class="fas fa-minus"></i></span>Barjeede sarwiisi kebinadi, formason dji lobbi</li>\
+                  <li><span class="icon"><i class="fas fa-minus"></i></span>Hebdè gollidibè walirobe tchède (kalissi)</li>\
+                  <li><span class="icon"><i class="fas fa-minus"></i></span>Balla et dowtol ley mawningol antarpiriisi djimone</li>\
+                  <li><span class="icon"><i class="fas fa-minus"></i></span>Batodone gollidibe mone ley Afrik fou.</li></ul>'
+      }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Voir les conditions d\'adhésion',
+        value: 'conditions'
+      },{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }
+    ]
+    })
+  }).then(function (res) {
+  if(res.value == 'conditions') {
+    conditions();
+  } else{
+    adhesion();
+  }
+  });
+}
+
+var conditions = function() {
+  botui.message.bot({
+    delay: 500,
+    type:'html',
+    content: '<ul><li><strong>Naatugol djabanagol</strong><br>Neddo e waldèrè fu na wawi filagol naddè cluster digital na djey nildu dè demande natirgol, hebita formulaire bindi. Gado dum fo naati ndè walderè.</li>\
+                  <li><strong>Adhésion de plein droit</strong><br>Les entreprises dotées d’une personnalité morale, ayant leurs activités qui relèvent du secteur du développement numérique et dont le siège ou un établissement se trouvent sur le territoire africain. Toutefois, l’adhésion est soumise à une ratification par le bureau exécutif à la majorité simple.</li></ul>'
+      }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Fornyude',
+        icon: 'angle-left',
+        value: 'skip'
+      }
+    ]
+    })
+  }).then(function (res) {
+  if(res.value == 'skip') {
+    adhesion();
+  } else{
+    adhesion();
+  }
+  });
+}
+
